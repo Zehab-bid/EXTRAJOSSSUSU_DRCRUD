a514e77127ab4b3592f00f3804864ebadb2cd335
Zehab-bid <zehabidha15@gmail.com>
2026-04-09 15:37:36 +0700

    langkah terakhir

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -15,7 +15,7 @@ namespace CRUDMahasiswaADO
     {
         private readonly SqlConnection conn;
         private readonly string connectionString =
-            "Data Source=DESKTOP-FHLKCTQ\\SQLEXPRESS";
+            "Data Source=DESKTOP-FHLKCTQ\\SQLEXPRESS;Initial Catalog=DBAkademikADO;Integrated Security=True";
 
         public FormMahasiswa()
         {
@@ -50,7 +50,17 @@ namespace CRUDMahasiswaADO
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            cmbJK.Items.Clear();
+            cmbJK.Items.Add("L");
+            cmbJK.Items.Add("P");
 
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.MultiSelect = false;
+            dataGridView1.ReadOnly = true;
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            dataGridView1.CellClick += dataGridView1_CellClick;
         }
 
         private void btnLoad_Click(object sender, EventArgs e)
@@ -278,5 +288,6 @@ namespace CRUDMahasiswaADO
             dtpTanggalLahir.Value = DateTime.Now;
             txtNIM.Focus();
         }
+
     }
 }
