687d025debccb0617aed78dc340f2a4890b64eab
Zehab-bid <zehabidha15@gmail.com>
2026-04-09 14:08:56 +0700

    komponen

--- CRUDMahasiswaADO/Form1.Designer.cs
+++ CRUDMahasiswaADO/Form1.Designer.cs
@@ -30,8 +30,15 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.txtNIM = new System.Windows.Forms.TextBox();
-            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbjk = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtpTanggalLahir = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAlamat = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -39,42 +46,106 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(56, 45);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.Size = new System.Drawing.Size(31, 16);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "NIM";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // txtNIM
             // 
-            this.txtNIM.Location = new System.Drawing.Point(136, 42);
+            this.txtNIM.Location = new System.Drawing.Point(158, 39);
             this.txtNIM.Name = "txtNIM";
             this.txtNIM.Size = new System.Drawing.Size(100, 22);
             this.txtNIM.TabIndex = 1;
             // 
-            // checkedListBox1
-            // 
-            this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(583, 170);
-            this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(120, 89);
-            this.checkedListBox1.TabIndex = 2;
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(433, 240);
+            this.label2.Location = new System.Drawing.Point(56, 92);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(44, 16);
             this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.label2.Text = "Nama";
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(158, 86);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(100, 22);
+            this.txtNama.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(56, 137);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 16);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Jenis Kelamin";
+            // 
+            // cmbjk
+            // 
+            this.cmbjk.FormattingEnabled = true;
+            this.cmbjk.Location = new System.Drawing.Point(158, 134);
+            this.cmbjk.Name = "cmbjk";
+            this.cmbjk.Size = new System.Drawing.Size(121, 24);
+            this.cmbjk.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(56, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Tanggal Lahir";
+            // 
+            // dtpTanggalLahir
+            // 
+            this.dtpTanggalLahir.Location = new System.Drawing.Point(158, 181);
+            this.dtpTanggalLahir.Name = "dtpTanggalLahir";
+            this.dtpTanggalLahir.Size = new System.Drawing.Size(200, 22);
+            this.dtpTanggalLahir.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(56, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Alamat";
+            // 
+            // txtAlamat
+            // 
+            this.txtAlamat.Location = new System.Drawing.Point(158, 221);
+            this.txtAlamat.Name = "txtAlamat";
+            this.txtAlamat.Size = new System.Drawing.Size(100, 22);
+            this.txtAlamat.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(56, 261);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Kode Prodi";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtAlamat);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dtpTanggalLahir);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.cmbjk);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNama);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.checkedListBox1);
             this.Controls.Add(this.txtNIM);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -88,8 +159,15 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtNIM;
-        private System.Windows.Forms.CheckedListBox checkedListBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbjk;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dtpTanggalLahir;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAlamat;
+        private System.Windows.Forms.Label label6;
     }
 }
 
