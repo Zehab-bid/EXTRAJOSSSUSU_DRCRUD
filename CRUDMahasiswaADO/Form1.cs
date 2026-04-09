dda81b2e7c586aef3bcd4c94f74e484e8943bed6
Zehab-bid <zehabidha15@gmail.com>
2026-04-09 14:42:12 +0700

    Method

--- CRUDMahasiswaADO/Form1.cs
+++ CRUDMahasiswaADO/Form1.cs
@@ -23,6 +23,26 @@ namespace CRUDMahasiswaADO
             conn = new SqlConnection(connectionString);
         }
 
+        private void ConnectDatabase()
+        {
+            try
+            {
+                if (conn.State == System.Data.ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                MessageBox.Show("Koneksi berhasil");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Koneksi gagal: " + ex.Message);
+            }
+        }
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            ConnectDatabase(); // Memanggil method yang sudah dibuat di atas
+        }
+
         private void label1_Click(object sender, EventArgs e)
         {
 
