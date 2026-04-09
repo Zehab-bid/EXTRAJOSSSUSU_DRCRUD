298ee847e588410f001f0d8d73e7d3802ecec052
Zehab-bid <zehabidha15@gmail.com>
2026-04-09 14:21:27 +0700

    TAMBAH BUTTON

--- CRUDMahasiswaADO/Form1.Designer.cs
+++ CRUDMahasiswaADO/Form1.Designer.cs
@@ -39,12 +39,20 @@
             this.label3 = new System.Windows.Forms.Label();
             this.txtAlamat = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
+            this.txtKodeProdi = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(56, 45);
+            this.label1.Location = new System.Drawing.Point(56, 25);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(31, 16);
             this.label1.TabIndex = 0;
@@ -53,15 +61,15 @@
             // 
             // txtNIM
             // 
-            this.txtNIM.Location = new System.Drawing.Point(158, 39);
+            this.txtNIM.Location = new System.Drawing.Point(158, 19);
             this.txtNIM.Name = "txtNIM";
-            this.txtNIM.Size = new System.Drawing.Size(100, 22);
+            this.txtNIM.Size = new System.Drawing.Size(315, 22);
             this.txtNIM.TabIndex = 1;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(56, 92);
+            this.label2.Location = new System.Drawing.Point(56, 53);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(44, 16);
             this.label2.TabIndex = 3;
@@ -69,15 +77,15 @@
             // 
             // txtNama
             // 
-            this.txtNama.Location = new System.Drawing.Point(158, 86);
+            this.txtNama.Location = new System.Drawing.Point(158, 47);
             this.txtNama.Name = "txtNama";
-            this.txtNama.Size = new System.Drawing.Size(100, 22);
+            this.txtNama.Size = new System.Drawing.Size(315, 22);
             this.txtNama.TabIndex = 4;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(56, 137);
+            this.label4.Location = new System.Drawing.Point(56, 78);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(90, 16);
             this.label4.TabIndex = 5;
@@ -86,15 +94,18 @@
             // cmbjk
             // 
             this.cmbjk.FormattingEnabled = true;
-            this.cmbjk.Location = new System.Drawing.Point(158, 134);
+            this.cmbjk.Items.AddRange(new object[] {
+            "L",
+            "P"});
+            this.cmbjk.Location = new System.Drawing.Point(158, 75);
             this.cmbjk.Name = "cmbjk";
-            this.cmbjk.Size = new System.Drawing.Size(121, 24);
+            this.cmbjk.Size = new System.Drawing.Size(315, 24);
             this.cmbjk.TabIndex = 6;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(56, 181);
+            this.label5.Location = new System.Drawing.Point(56, 105);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(90, 16);
             this.label5.TabIndex = 7;
@@ -102,15 +113,15 @@
             // 
             // dtpTanggalLahir
             // 
-            this.dtpTanggalLahir.Location = new System.Drawing.Point(158, 181);
+            this.dtpTanggalLahir.Location = new System.Drawing.Point(158, 105);
             this.dtpTanggalLahir.Name = "dtpTanggalLahir";
-            this.dtpTanggalLahir.Size = new System.Drawing.Size(200, 22);
+            this.dtpTanggalLahir.Size = new System.Drawing.Size(315, 22);
             this.dtpTanggalLahir.TabIndex = 8;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(56, 221);
+            this.label3.Location = new System.Drawing.Point(56, 133);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(49, 16);
             this.label3.TabIndex = 9;
@@ -118,25 +129,95 @@
             // 
             // txtAlamat
             // 
-            this.txtAlamat.Location = new System.Drawing.Point(158, 221);
+            this.txtAlamat.Location = new System.Drawing.Point(158, 133);
+            this.txtAlamat.Multiline = true;
             this.txtAlamat.Name = "txtAlamat";
-            this.txtAlamat.Size = new System.Drawing.Size(100, 22);
+            this.txtAlamat.Size = new System.Drawing.Size(315, 45);
             this.txtAlamat.TabIndex = 10;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(56, 261);
+            this.label6.Location = new System.Drawing.Point(56, 184);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(74, 16);
             this.label6.TabIndex = 11;
             this.label6.Text = "Kode Prodi";
+            // 
+            // txtKodeProdi
+            // 
+            this.txtKodeProdi.Location = new System.Drawing.Point(158, 184);
+            this.txtKodeProdi.Name = "txtKodeProdi";
+            this.txtKodeProdi.Size = new System.Drawing.Size(315, 22);
+            this.txtKodeProdi.TabIndex = 12;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(59, 250);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(682, 177);
+            this.dataGridView1.TabIndex = 13;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(539, 18);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(152, 24);
+            this.btnConnect.TabIndex = 14;
+            this.btnConnect.Text = "Membuka Koneksi";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(539, 46);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(152, 24);
+            this.btnLoad.TabIndex = 15;
+            this.btnLoad.Text = "Menampilkan Data";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(539, 78);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 24);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Menambah Data";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(539, 107);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 24);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Mengubah Data";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(539, 137);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(152, 24);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Menghapus Data";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtKodeProdi);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.txtAlamat);
             this.Controls.Add(this.label3);
@@ -150,6 +231,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -168,6 +250,13 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtAlamat;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtKodeProdi;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
